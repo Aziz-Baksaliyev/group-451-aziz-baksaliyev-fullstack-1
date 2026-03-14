b47227546161ca4cfa8143cbc8d9f3baae7bda2e
Aziz-Baksaliyev <nataoneorucova@gmail.com>
2026-03-14 20:02:59 +0400

    9 ое видео

---
+++ store/src/main/java/com/my/store/repository/ProductRepository.java
@@ -0,0 +1,9 @@
+package com.my.store.repository;
+
+import com.my.store.model.Product;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ProductRepository extends JpaRepository<Product, Long> {
+
+    void create(Product product);
+}
