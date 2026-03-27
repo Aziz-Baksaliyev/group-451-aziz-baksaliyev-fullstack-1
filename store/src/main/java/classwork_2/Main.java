a62de56bf7e6b0149b725d415ac449557b3c187b
Aziz-Baksaliyev <nataoneorucova@gmail.com>
2026-03-27 20:33:48 +0400

    Create Main.java

---
+++ store/src/main/java/classwork_2/Main.java
@@ -0,0 +1,64 @@
+package classwork_2;
+
+public class Main {
+    public static void main(String[] args) {
+//        1)
+//        int num = 2;
+//        if (num > 0) {
+//            System.out.println("Положительное число");
+//        } else if (num == 0) {
+//            System.out.println("Ноль");
+//        }
+//        else {
+//            System.out.println("Отрицательное число");
+//        }
+//        2)
+//            int num = 4;
+//            if (num%2==0) {
+//                System.out.println("Четное");
+//            }
+//            else {
+//                System.out.println("Нечетное");
+//            }
+//        3)
+//          int a = 9;
+//          int b = 5;
+//          if (a > b) {
+//              System.out.println(a + " больше чем " + b);
+//          } else if (a < b) {
+//              System.out.println(a + " меньше чем " + b);
+//          }
+//        4)
+//            int age = 19;
+//            if (age < 18) {
+//                System.out.println("Несовершеннолетний");
+//            }
+//            else if (age >= 18) {
+//                System.out.println("Совершеннолетний");
+//            }
+//        5)
+//            int num = 99;
+//            if (num > 90 && num <= 100) {
+//                System.out.println("Отлично");
+//            }
+//            else if (num > 70 && num <= 89) {
+//                System.out.println("Хорошо");
+//            }
+//            else if (num > 50 && num <= 69) {
+//                System.out.println("Удовлетворительно");
+//            }
+//            else {
+//                System.out.println("Не сдал");
+//            }
+//        6)
+        //...
+//        7)
+//            int year = 365;
+//            if (year == 366) {
+//                System.out.println("Високосный год");
+//            }
+//            else if (year == 365) {
+//                System.out.println("Обычный год");
+//            }
+    }
+}
