a8d112586b6699a8961bac1830fa12db85203be0
Aziz-Baksaliyev <nataoneorucova@gmail.com>
2026-04-28 21:58:09 +0400

    практ задания

---
+++ store/src/main/java/classwork_6/Tasks.java
@@ -0,0 +1,70 @@
+package classwork_6;
+
+public class Tasks {
+    public static void main (String[] args) {
+//       1) System.out.println("Привет, Мир!");
+//       2) for (int i = 1; i <= 10; i++) {
+//            System.out.println(i);
+//        }
+//       3) int age = 13;
+//        int afterTenYears = 10;
+//        age += afterTenYears;
+//        System.out.println(age);
+//       4) int a = 12;
+//        if (a % 2 == 0) {
+//            System.out.println("Even");
+//        }
+//        else {
+//            System.out.println("Odd");
+//        }
+//       5) int sum = 0;
+//        for (int i = 1; i <= 100; i++) {
+//            sum += i;
+//            System.out.println(sum);
+//        }
+//       7) int number = 3;
+//        for (int i = 1; i <= 10; i++) {
+//            System.out.println(number + " × " + i + " = " + (number * i));
+//        }
+//               8) System.out.println("Максимум: " + max3(10, 25, 5)); // Выведет 25
+//                System.out.println("Максимум: " + max3(-1, 0, -5));  // Выведет 0
+//            }
+//
+//            public static int max3(int a, int b, int c) {
+//                int max;
+//
+//                if (a >= b && a >= c) {
+//                    max = a;
+//                } else if (b >= a && b >= c) {
+//                    max = b;
+//                } else {
+//                    max = c;
+//                }
+//                return max;
+
+//               9) printNameInfo("Александр");
+//            }
+//
+//            public static void printNameInfo(String name) {
+//                int length = name.length();
+//
+//                String upperName = name.toUpperCase();
+//
+//                System.out.println("Количество букв в имени: " + length);
+//                System.out.println("Имя заглавными буквами: " + upperName);
+
+//    10) int height = 5;
+//        for (int i = 1; i <= height; i++) {
+//            for (int j = 1; j <= i; j++) {
+//                System.out.print("*");
+//            }
+//            System.out.println();
+//        }
+//       11) int[] favoriteNumbers = {7, 13, 42, 64, 128};
+//
+//        for (int number : favoriteNumbers) {
+//            System.out.println(number);
+//        }
+
+    }
+        }
