85123a102a41de62d22fa9f51d2540863bb9a099
Aziz-Baksaliyev <nataoneorucova@gmail.com>
2026-05-05 21:55:41 +0400

    Create SecondMain.java

---
+++ store/src/main/java/classwork_8/SecondMain.java
@@ -0,0 +1,54 @@
+package classwork_8;
+
+public class SecondMain {
+    public static void main(String[] args) {
+//        for (int i = 1; i <= 10; i++) {
+//            System.out.print(i + " ");
+//        }
+//        for (int i = 2; i <= 20; i += 2) {
+//            System.out.print(i + " ");
+//        }
+//        int sum = 0;
+//        for (int i = 1; i <= 100; i++) {
+//            sum += i;
+//        }
+//        System.out.println(sum);
+//        int num = 7;
+//        for (int i = 1; i <= 10; i++) {
+//            System.out.println(num + " x " + i + " = " + (num * i));
+//        }
+//        int i = 10;
+//        while (i >= 1) {
+//            System.out.print(i + " ");
+//            i--;
+//        }
+//        int number = 58342;
+//        int count = 0;
+//
+//        if (number == 0) {
+//            count = 1;
+//        } else {
+//            while (number > 0) {
+//                count++;
+//                number /= 10;
+//            }
+//        }
+//
+//        System.out.println("Количество цифр: " + count);
+//        int number = 4567;
+//        int sum = 0;
+//
+//        while (number > 0) {
+//            sum += number % 10;
+//            number /= 10;
+//        }
+//
+//        System.out.println(sum);
+//        int choice = 3;
+//        do {
+//            System.out.println("1. Начать игру");
+//            System.out.println("2. Настройки");
+//            System.out.println("3. Выйти");
+//        } while (choice != 3);
+    }
+}
