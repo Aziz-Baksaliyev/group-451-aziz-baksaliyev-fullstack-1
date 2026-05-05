075474f94b253f5489497291558438c51455f016
Aziz-Baksaliyev <nataoneorucova@gmail.com>
2026-05-05 21:04:56 +0400

    Это классворк (правда забыл свитч...)

---
+++ store/src/main/java/classwork_8/Main.java
@@ -0,0 +1,85 @@
+package classwork_8;
+
+public class Main {
+    public static void main(String[] args) {
+//        1.
+//        int a = -7;
+//        if (a > 0) {
+//            System.out.println("Число положительное");
+//        } else if (a < 0) {
+//            System.out.println("Число отрицательное");
+//        } else {
+//            System.out.println("Число равно нулю");
+//        }
+//            2.
+//            int isOddOrEven = 14;
+//            if (isOddOrEven % 2 == 0) {
+//                System.out.println("Четное число");
+//            } else {
+//                System.out.println("Нечетное число");
+//            }
+//        3.
+//            int a = 25;
+//            int b = 31;
+//            if (a > b) {
+//                System.out.println(a + " больше " + b);
+//            } else if (a < b) {
+//                System.out.println(a + " меньше " + b);
+//            }
+//        4.
+//        int age = 17;
+//        if (age >= 18) {
+//            System.out.println("Access granted.");
+//        } else {
+//            System.out.println("Access denied!");
+//        }
+//        5.
+//        int score = 82;
+//        if (score >= 90 && score <= 100) {
+//            System.out.println("Отлично!");
+//        } else if (score >= 70 && score < 89) {
+//            System.out.println("Хорошо.");
+//        } else if (score >= 50 && score < 69) {
+//            System.out.println("Удовлетворительно.");
+//        } else if (score >= 0 && score < 49) {
+//            System.out.println("Не сдал!");
+//        } else if (score > 100 || score < 0) {
+//            System.out.println("Некорректный балл");
+//        }
+//        6.
+//        String login = "admin";
+//        String password = "12345";
+//        if (login.equals("admin") && password.equals("12345")) {
+//            System.out.println("Вход выполнен.");
+//        } else {
+//            System.out.println("Вход не выполнен.");
+//        }
+//        7.
+//        int temperature = 32;
+//        if (temperature < 0) {
+//            System.out.println("Очень холодно.");
+//        } else if (temperature >= 0 && temperature <= 15) {
+//            System.out.println("Прохладно.");
+//        } else if (temperature > 15 && temperature <= 30) {
+//            System.out.println("Тепло.");
+//        } else if (temperature > 30) {
+//            System.out.println("Жарко.");
+//        }
+//        8.
+//        int day = 3;
+//        switch -> ()
+//        я забыл 😭
+//        9.
+//        int a = 20;
+//        int b = 5;
+//        char operator = '*';
+//        if (10)
+//        10.
+//        int number = 55;
+//        if (number >= 10 && number <= 100) {
+//            System.out.println("Число входит в диапазон.");
+//        } else {
+//            System.out.println("Число вне диапазона.");
+//        }
+    }
+}
